58c8a8427658f391e0ed1ed383e9bae4c8c49c61
Roman Litsiiuk <alexandrovich2015@gmail.com>
2018-04-24 21:39:57 +0300

    Last script was modified

--- scriptLast.js
+++ scriptLast.js
@@ -1,41 +1,45 @@
 function userArrayAction() {
-    var userString = prompt('Введите ряд чисел через запятую', '');
-    var dirtyArray = userString.split(',');
     var i;
     var userArray = [];
-    console.log(dirtyArray);
+    var userNumber;
     var countEven = 0;
     var countOdd = 0;
     var evenSum = 0;
     var oddCalc = 1;
     var minVal;
     var maxVal;
+    var userString = prompt('Введите ряд чисел через запятую', '');
+    var dirtyArray = userString.split(',');
+    console.log(dirtyArray);
 
-    for (i = 0; i < dirtyArray.length; i++) {
-        userArray.push(dirtyArray[i].replace(/[^0-9\-]+/g, ""));
+    for (i = dirtyArray.length - 1; i >= 0; i--) {
+        userNumber = dirtyArray[i].replace(/[^0-9\-]+/g, "");
+        if (userNumber !== '') {
+            userArray.unshift(+userNumber);
+        }
     }
     console.log(userArray);
 
     for (i = 0; i < userArray.length; i++) {
-        if (userArray[i] % 2 === 0 && userArray[i] != 0) {
+        if (userArray[i] % 2 === 0) {
             countEven += 1;
-            evenSum += +userArray[i];
+            evenSum += userArray[i];
         }
         if (userArray[i] % 2 === 1) {
             countOdd += 1;
-            oddCalc *= +userArray[i];
+            oddCalc *= userArray[i];
         }
     }
 
     minVal = userArray[0];
     maxVal = userArray[0];
     for (i = 0; i < userArray.length; i++) {
-        if(minVal >= userArray[i] && userArray[i] != '') {
-            minVal = +userArray[i];
+        if(minVal >= userArray[i]) {
+            minVal = userArray[i];
         }
 
         if(maxVal <= userArray[i]) {
-            maxVal = +userArray[i];
+            maxVal = userArray[i];
         }
     }
 
